e83cc275d543e5c43fa787abbb2f5890ec4aea7e
Vincent <cottalorda.vincent@gmail.com>
2021-10-06 15:55:05 +0200

    ajout du caroussel pour packetage

--- asset/js/carousel.js
+++ asset/js/carousel.js
@@ -1,48 +1,8 @@
-class Carousel {
-  /**
-   *
-   * @param {HTMLelement} element
-   * @param {object} options
-   * @param {object} options.slidesToScroll Nombre d'élément à scroller
-   * @param {object} options.slidesVisible Nombres de slides Visibles
-   */
-  constructor(element, options = {}) {
-    this.element = element;
-    this.options = Object.assign(
-      {},
-      {
-        slidesToScroll: 1,
-        slidesVisible: 1,
-      },
-      options
-    );
-
-    this.children = [].slice.call(element.children);
-    let root = this.createDivWithClass("carousel");
-    let container = this.createDivWithClass("carousel__container");
-    root.appendChild(container);
-    this.element.appendChild(root);
-    this.children.array.forEach((child) => {
-      let item = this.createDivWithClass("carousel__item");
-      item.appendChild(child);
-      container.appendChild(item);
-    });
-  }
-  /**
-   *
-   * @param {string} className
-   * @returns {HTMLElement}
-   */
-  createDivWithClass(className) {
-    let div = document.createElement("div");
-    div.setAttribute("class", className);
-    return div;
-  }
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  new Carousel(document.querySelector("#carousel1"), {
-    slidesToScroll: 1,
-    slidesVisible: 1,
+$(document).ready(function () {
+  $(".carousel-package").slick({
+    arrows: true,
+    mobileFirst: true,
+    prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
   });
 });

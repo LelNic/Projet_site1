8af768c217623cc87b79e3c8e80c3c8d08a56686
Vincent <cottalorda.vincent@gmail.com>
2021-10-07 16:11:19 +0200

    Finalisation de tout les containers de Slides

--- asset/js/carousel.js
+++ asset/js/carousel.js
@@ -15,3 +15,21 @@ $(document).ready(function () {
     nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
   });
 });
+
+$(document).ready(function () {
+  $(".carousel-history").slick({
+    arrows: true,
+    mobileFirst: true,
+    prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+  });
+});
+
+$(document).ready(function () {
+  $(".carousel-reconstitution").slick({
+    arrows: true,
+    mobileFirst: true,
+    prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+  });
+});
